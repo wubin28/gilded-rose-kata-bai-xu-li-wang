9efddf89f9ed157b95b1db17a3cef359b91995a4
wolf_CC147933E0 <wolf@cyber-dojo.org>
2015-11-24 17:15:59 +0000

    12

--- sandbox/ItemTest.java
+++ sandbox/ItemTest.java
@@ -5,13 +5,14 @@ public class ItemTest {
 
     @Test
     public void after_update_lower_both_values_at_each_day(){
+        //Given
         Item item = new Item("normal",14,10);
+
+        //When
         item.updateQuality();
-        
+
+        //Then
         assertEquals(13,item.getSellIn());
         assertEquals(9,item.getQuality());
-        
     }
-
- 
 }
